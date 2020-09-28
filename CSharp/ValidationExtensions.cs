932c0369d522fb95d8f417ee083236a191cf1f20
skerdi berberi <skerdi19berberi@gmail.com>
2020-09-28 02:51:01 +0200

    CSharp: Added Email and NID validation helpers

--- CSharp/ValidationExtensions.cs
+++ CSharp/ValidationExtensions.cs
@@ -43,4 +43,98 @@ public static class ValidationExtensions
         return type.IsSerializable;
     }
 
+
+    #region EmailRegex
+    /// <summary>
+    /// Checks if input is a valid email adress format
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns></returns>
+    public static bool IsValidEmail(this string email)
+    {
+        if (email.IsEmpty())
+            return false;
+
+        try
+        {
+            // Normalize the domain
+            email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                  RegexOptions.None, TimeSpan.FromMilliseconds(200));
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        try
+        {
+            return Regex.IsMatch(email,
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Examines the domain part of the email and normalizes it.
+    /// </summary>
+    /// <param name="match"></param>
+    /// <returns></returns>
+    private static string DomainMapper(Match match)
+    {
+        // Use IdnMapping class to convert Unicode domain names.
+        var idn = new IdnMapping();
+
+        // Pull out and process domain name (throws ArgumentException on invalid)
+        var domainName = idn.GetAscii(match.Groups[2].Value);
+
+        return match.Groups[1].Value + domainName;
+    }
+    #endregion
+
+    /// <summary>
+    /// Checks if the input is a valid Albanian NID/NUIS
+    /// </summary>
+    /// <param name="nid"></param>
+    /// <returns></returns>
+    public static bool IsValidNID(this string nid)
+    {
+        if (nid.IsEmpty())
+            return false;
+
+        if (nid.Length != 10)
+            return false;
+
+        try
+        {
+            return Regex.IsMatch(nid,
+                @"^[a-zA-Z]\d{8}[a-zA-Z]",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
 }
