2da983ddb413d3f7c204c0ea14c2de57af3dace3
skerdi berberi <skerdi19berberi@gmail.com>
2020-09-29 21:58:15 +0200

    refactoring

--- CSharp/ValidationExtensions.cs
+++ CSharp/ValidationExtensions.cs
@@ -5,14 +5,16 @@ public static class ValidationExtensions
     /// </summary>
     /// <param name="str">string input</param>
     /// <returns>boolean value if string is empty or not</returns>
-    public static bool IsEmpty(this string str) => string.IsNullOrWhiteSpace(str) || string.Empty == str;
+    public static bool IsEmpty(this string str)
+                        => string.IsNullOrWhiteSpace(str) || string.Empty == str;
 
     /// <summary>
     /// Checks if guid is valid
     /// </summary>
     /// <param name="guid"></param>
     /// <returns></returns>
-    public static bool IsValidGuid(this Guid guid) => guid != null && guid != Guid.Empty;
+    public static bool IsValidGuid(this Guid guid)
+                        => guid != null && guid != Guid.Empty;
 
     /// <summary>
     /// Checks if filename is valid
